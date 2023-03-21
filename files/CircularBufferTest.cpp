e5fdca22ec5d8b12851cb738a42fa9ad50d629db
rCbPhl <rCbPhl@cyber-dojo.org>
2023-03-21 12:33:35 +0000

    63 ran tests, no prediction, got red

--- files/CircularBufferTest.cpp
+++ files/CircularBufferTest.cpp
@@ -69,7 +69,7 @@ TEST(CircularBuffer, is_empty_after_creation)
 
 TEST(CircularBuffer, is_not_full_after_creation)
 {
-    CHECK_FALSE(CircularBuffer_IsFull(buffer));
+    //CHECK_FALSE(CircularBuffer_IsFull(buffer));
 }
 
 /* PLEASE DELETE PREVIOUS COMMENTS AS YOU GET TESTS TO PASS */
