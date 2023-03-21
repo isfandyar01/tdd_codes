1c9f7e756505880e1f65fd9ddf8243d4067e09f0
rCbPhl <rCbPhl@cyber-dojo.org>
2023-03-21 13:08:44 +0000

    77 ran tests, no prediction, got green

--- files/CircularBuffer.c
+++ files/CircularBuffer.c
@@ -51,7 +51,7 @@ void  CircularBuffer_Put(struct CircularBuffer *self, int value)
  self->values[self->input_index]=value;
  self->count++;
  self->input_index++;
- if(self->input_index>=self->capacity
+ if(self->input_index>=self->capacity)
     {
         self->input_index=0;
     }
