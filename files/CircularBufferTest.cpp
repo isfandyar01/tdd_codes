1c7ccaa6fa4406e1a3e8f3f26996ba8ae5d1b19e
rCbPhl <rCbPhl@cyber-dojo.org>
2023-03-22 18:19:54 +0000

    83 ran tests, no prediction, got red

--- files/CircularBufferTest.cpp
+++ files/CircularBufferTest.cpp
@@ -396,7 +396,9 @@ TEST(CircularBuffer, force_a_buffer_wraparound)
     CircularBuffer_Put(buffer, 1);
     CircularBuffer_Put(buffer, 2);
     CircularBuffer_Put(buffer, 3);
-    CircularBuffer_Get(buffer);
+     CircularBuffer_Get(buffer);
+     CircularBuffer_Get(buffer);
+     CircularBuffer_Get(buffer);
     CircularBuffer_Put(buffer, 5);
     LONGS_EQUAL(55, CircularBuffer_Get(buffer));
   //  CHECK(CircularBuffer_IsEmpty(buffer));
